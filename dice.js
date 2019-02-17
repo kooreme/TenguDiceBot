e21d87af9227520b29d83ed279d5de9a5db382d4
kooreme <kooreme@hotmail.co.jp>
2019-02-17 22:57:13 +0900

    dicerolltest

--- dice.js
+++ dice.js
@@ -1,5 +1,21 @@
 //dice.js
 
 exports.receiveDiceRoll = function(string) {
-    return 'I received ' + '"' + string + '"';
+    var str = diceRoll(5,6).join;
+    return 'diceroll! : (' + str + ')';
+//    return 'I received ' + '"' + string + '"';
+};
+
+function diceRoll(sum, diceMen) {
+    var result = [];
+    for(i = 0;i < sum;i++) {
+        result.push(getRandomIntInclusive(1,diceMen));
+    }
+    return result;
+};
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 };
