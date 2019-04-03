55a179e1bd6aec0830e77dd8b70ff1b43645dd93
kooreme <kooreme@hotmail.co.jp>
2019-04-03 17:48:23 +0900

    ktq upd

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 
 const dice = require('./diceroll');
 const kt_dice = require('./kt_diceroll');
+const kt_quest = require('./ktq_quest');
 const Log = require('./log.js');
 
 client.on('ready', () => {
@@ -15,6 +16,8 @@ client.on('ready', () => {
 //メッセージ反応部
 const REACT_REGEXP = /^\/nd /g;
 const REACT_REGEXP_KATAMICHI = /^\/kd /g;
+const REACT_REGEXP_KTQUEST = /^\/ktq /g;
+
 
 client.on('message', message => {
     let content = message.content;
@@ -30,6 +33,12 @@ client.on('message', message => {
         content = content.replace(/\/kd /, '');
 
         message.reply(kt_dice.receiveDiceRoll(content));
+        Log.prints('content : ' + content);
+    }
+    else if (content.search(REACT_REGEXP_KTQUEST) !== -1) {
+        content = content.replace(/\/ktq /, '');
+
+        message.reply(kt_quest.receiveQuest(content));
         Log.prints('content : ' + content);
     }
 
