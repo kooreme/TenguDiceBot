3549eab0f889874eb91b3d1ed728301e3cbc6dfd
kooreme <kooreme@hotmail.co.jp>
2019-12-27 01:07:27 +0900

    postgres updated

--- postgres.js
+++ postgres.js
@@ -1,15 +1,18 @@
 /* eslint-disable no-undef */
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const pgClient = new Client({
+const pgPool = new Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: 5432,
+    max: 20,
 });
 
+let instance 
+
 exports.connect = function() {
-    pgClient.connect();
+    pgPool.connect();
 }
 
