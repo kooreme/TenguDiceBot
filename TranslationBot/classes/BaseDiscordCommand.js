98977b6602ec0240f592fcdb5dc22c8c5b613e06
CallMe AsYouFeel <75649254+cm-ayf@users.noreply.github.com>
2022-03-05 16:26:57 +0900

    richer BaseDiscordCommand jsdoc

--- TranslationBot/classes/BaseDiscordCommand.js
+++ TranslationBot/classes/BaseDiscordCommand.js
@@ -1,10 +1,23 @@
+/** @typedef {import('discord.js').ApplicationCommandData} ApplicationCommandData */
+/** @typedef {import("discord.js").CommandInteraction} CommandInteraction */
+
 /**
+ * @class
  * コマンド定義を行うクラスの継承元です．
  */
 class BaseDiscordCommand {
+    /** @type {ApplicationCommandData} コマンド定義です． */
     command;
+
     constructor() {}
-    async execute() {
+
+    /**
+     * コマンドを実行する関数です．
+     * @param {CommandInteraction} interaction コマンドインタラクションです．
+     * @throws 関数が実装されていなければ，エラーを投げます．
+     */
+    async execute(interaction) {
+        interaction;
         throw new Error("No Implement.");
     }
 }
