ec946adaba696752a914acc13e5313c1af7b3bd6
kooreme <kooreme@hotmail.co.jp>
2022-11-07 16:34:21 +0000

    依存パッケージの最適化、翻訳botのコメントアウト

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const TENGU_MENTENANCE = Number(process.env.TENGU_MENTENANCE) === 1 ?? false;
-const TRANS_MENTENANCE = Number(process.env.TRANS_MENTENANCE) === 1 ?? false;
-if(!TENGU_MENTENANCE) require("./TenguDiceBot/index");
-if(!TRANS_MENTENANCE) require("./TranslationBot/index");
+//const TRANS_MENTENANCE = Number(process.env.TRANS_MENTENANCE) === 1 ?? false;
+if (!TENGU_MENTENANCE) require("./TenguDiceBot/index");
+//if(!TRANS_MENTENANCE) require("./TranslationBot/index");
--- register.js
+++ register.js
@@ -1,4 +1,4 @@
 const IS_TENGU_REGISTER = Number(process.env.IS_TENGU_REGISTER) === 1 ?? false;
-const IS_TRANS_REGISTER = Number(process.env.IS_TRANS_REGISTER) === 1 ?? false;
-if(!IS_TENGU_REGISTER) require("./TenguDiceBot/register");
-if(!IS_TRANS_REGISTER) require("./TranslationBot/register");
+//const IS_TRANS_REGISTER = Number(process.env.IS_TRANS_REGISTER) === 1 ?? false;
+if (!IS_TENGU_REGISTER) require("./TenguDiceBot/register");
+//if(!IS_TRANS_REGISTER) require("./TranslationBot/register");
